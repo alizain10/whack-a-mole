9bff737c8b760d695d321b825350d8d339e1510f
alizain10 <71589343+alizain10@users.noreply.github.com>
2020-12-30 18:38:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ square.forEach(id => {
 
 function moveMole() {
   let timerId = null
-  timerId = setInterval(randomSquare, 500)
+  timerId = setInterval(randomSquare, 1000)
 }
 
 moveMole()
@@ -42,7 +42,7 @@ function countDown() {
 
   if(currentTime === 0 ) {
     clearInterval(timerId)
-    alert('GAME OVER! Your final score is' + result)
+    alert('GAME OVER! Your final score is ' + result)
   }
 }
 
